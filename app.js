a78ba6efe3d66f15584d4d3435df5ad9f79bf79e
Kang Hyewon <lain13@gmail.com>
2025-07-20 22:09:07 +0900

    Local work while others are working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log('Hello World');
 function login() { console.log('Login function'); }
 function logout() { console.log('Logout function'); }
-function getUser() { return 'current user'; }
+function getUser() { return 'current user'; }function localFeature() { console.log('Local work'); }
