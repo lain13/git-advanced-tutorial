77dc521d6587bb9501048cd25cb9fb44504806e6
Kang Hyewon <lain13@gmail.com>
2025-07-20 22:00:49 +0900

    Add getUser function with different implementation

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Hello World');
 function login() { console.log('Login function'); }
 function logout() { console.log('Logout function'); }
+function getUser() { return 'current user'; }
